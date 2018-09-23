620e6df2823f2ee9e5591f7f99a61fff3d8cbc10
antoniogomez2015 <monoproduccion@gmail.com>
2018-09-23 12:27:15 -0430

    se ha inicializado el repositorio de Vuejs, para su estudio y desarrollo, 23 de Septiembre del 2018

---
+++ 00_example.js
@@ -0,0 +1,32 @@
+new Vue({
+
+    el: '#formulario',
+
+    data: {
+        nombre: '',
+        edad: '',
+        sexo: '',
+        correo: ''
+    }
+
+});
+
+if<->{
+    if(<->){
+        if<->){
+
+            }
+    }
+}
+
+    
+const n1 = "Tony";
+
+n1.documentGetElementById('aver');
+n1.addEventListener('click', function() {
+
+    alert('a ver..');
+
+});
+document.getElementById('demo1');
+var proof = 45;
