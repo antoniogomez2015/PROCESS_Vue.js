599daa566f8c2479df30e30bcea369b67db17132
tonyGomez <monoproduccion.com>
2019-05-13 07:36:24 +0200

    Se ha comenzado con el curso de Vuejs, 13 de Mayo del 2019

--- Js/dev.js
+++ Js/dev.js
@@ -2,7 +2,17 @@ const app = new Vue({
 
     el: '#main',
     data: {
-        titulo: 'Bienvenido al mundo de VueJs'
+        titulo: 'Bienvenido al mundo de VueJs',
+        frutas: ["manzana", "pera", "platano"],
+        user: {
+            name: "tony",
+            lastName: "Gomez",
+            eged: 38
+        },
+        history: [{lebel: "Cargo", cargo: "designer"}, {lebel2: "Antiguedad", aged: 5}, {lebel3: "Rendimiento", performance: "senior"}]
     }
 
 })
+
+
+
