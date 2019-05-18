160bee1d31c960d5b4f9bea70493058cde2b8ca9
tonyGomez <monoproduccion.com>
2019-05-18 09:27:53 +0200

    Se ha continuado con el curso de Vue, 18 de Mayo del 2019

---
+++ Js/app_demo1.js
@@ -0,0 +1,37 @@
+const app = new Vue({
+
+    el: '#app',
+    data: {
+        titulo: 'Aplicación con VueJs',
+        frutas: [
+            {nombre: "Manzanas", cantidad: 10},
+            {nombre: "Peras", cantidad: 2},
+            {nombre: "Cambures", cantidad: 0}
+        ],
+        newElement: "",
+        total: 0
+    },
+    methods: {
+        addEl(){
+            this.frutas.push({
+                nombre: this.newElement, cantidad: 0               
+            });
+            return this.newElement = ""           
+        }
+    },
+    computed: {
+        calcule(){
+            this.total = 0;
+            for(el of this.frutas){
+                this.total = this.total + el.cantidad;
+            };
+            return this.total;
+        }
+    }
+
+})
+
+
+
+
+
